784dc876c0cb7de1e74d9522b6add5e9cd6c6e12
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-26 19:25:42 +0900

    Added ball shape respect to cameras orientation and location. Added physics behavior to ball and basket in order to shoot and score

--- AR_Hoops/ViewController.swift
+++ AR_Hoops/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
+    var startingPosition: SCNNode?
+    var power: Float = 0
     let configuration = ARWorldTrackingConfiguration()
+    var basketAdded: Bool {
+        return self.sceneView.scene.rootNode.childNode(withName: "Basket", recursively: false) != nil
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
@@ -30,10 +35,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let tapLocation = sender.location(in: sceneView)
         let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
         if !hitTest.isEmpty{
-            self.addItem(hitTestResult: hitTest.first!)
+            self.addBasket(hitTestResult: hitTest.first!)
         }
     }
     
+    func addBasket(hitTestResult: ARHitTestResult){
+        let scene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
+        let basketNode = scene!.rootNode.childNode(withName: "Basket", recursively: false)!
+        let transform = hitTestResult.worldTransform
+        let planePosition = transform.columns.3
+        basketNode.position = SCNVector3(planePosition.x,planePosition.y,planePosition.z)
+        basketNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: basketNode, options: [SCNPhysicsShape.Option.keepAsCompound: true, SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]))
+        self.sceneView.scene.rootNode.addChildNode(basketNode)
+        
+    }
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard anchor is ARPlaneAnchor else {return}
         DispatchQueue.main.async{
@@ -44,16 +59,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         
     }
-    
-    func addItem(hitTestResult: ARHitTestResult){
-        let scene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
-        let basketballNode = scene!.rootNode.childNode(withName: "Basket", recursively: false)!
-        let transform = hitTestResult.worldTransform
-        let planePosition = transform.columns.3
-        basketballNode.position = SCNVector3(planePosition.x,planePosition.y,planePosition.z)
-        self.sceneView.scene.rootNode.addChildNode(basketballNode)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.basketAdded == true {
+            guard let pointOfView = self.sceneView.pointOfView else {return}
+            self.power = 10
+            let transform = pointOfView.transform
+            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+            let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+            let position = location + orientation
+            let ball = SCNNode(geometry: SCNSphere(radius: 0.1))
+            ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Ball")
+            ball.position = position
+            let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
+            ball.physicsBody = body
+            ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
+            self.sceneView.scene.rootNode.addChildNode(ball)
+    }
         
     }
-
+    
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
